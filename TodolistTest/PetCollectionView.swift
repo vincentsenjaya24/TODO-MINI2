073a3f6be40158485865aab075911f908cff594d
Vincent Senjaya <73159146+vincentsenjaya24@users.noreply.github.com>
2024-06-26 03:43:33 +0700

    pet gallery and main view

---
+++ TodolistTest/PetCollectionView.swift
@@ -0,0 +1,18 @@
+//
+//  PetCollectionView.swift
+//  TodolistTest
+//
+//  Created by Vincent Senjaya on 26/06/24.
+//
+
+import SwiftUI
+
+struct PetCollectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PetCollectionView()
+}
